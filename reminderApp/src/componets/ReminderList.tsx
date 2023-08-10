abdb4369b8eb9dc84fa9e3f0b208d8415ae5b08d
aayala4569 <angelicaayala100@gmail.com>
2023-08-10 16:30:55 -0700

    added a font to my list and color to my delete btn

--- reminderApp/src/componets/ReminderList.tsx
+++ reminderApp/src/componets/ReminderList.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Reminder from "../Models/reminder";
 
-
 interface ReminderListProps {
   items: Reminder[];
-  onRemoveReminder: (id: number) => void
+  onRemoveReminder: (id: number) => void;
 }
 const ReminderList = ({ items, onRemoveReminder }: ReminderListProps) => {
   return (
-
     <ul className="list-group">
       {items.map((item) => (
-        <li className="list-group-item d-flex justify-content-between text-white"  key={item.id}>
+        <li
+          className="list-group-item d-flex justify-content-between text-white"
+          key={item.id}
+        >
           {item.title}
-          <button className="btn btn-outline-info round-pill" onClick={() => onRemoveReminder(item.id)}>Delete</button>
+          <button
+            className="btn btn-outline-info round-pill"
+            onClick={() => onRemoveReminder(item.id)}
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
